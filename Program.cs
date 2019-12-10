60a55a2a42d882f2f220401e246a311f1d25f9e4
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2019-12-11 06:55:54 +1000

    Added file serilaization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace AuthorSerialize
@@ -14,6 +15,19 @@ namespace AuthorSerialize
 
             string str = JsonConvert.SerializeObject(author,Formatting.Indented);
             System.Console.WriteLine(str);
+
+            //serialize to a file --> serialize means to convert an object 
+            //representation in memory to a representation on disk/wire. In this casse the 
+            //object representation is being stored on disk as a file
+
+            //Serialization is differnt than converting the properties of a type to string
+            //and writing them to a file. Here the responsibility of storing the memory based
+            //representation of the object to disk belongs to the object itself. 
+            using (StreamWriter file = File.CreateText(@".\author.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, author);
+            }
 
 
         }
